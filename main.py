e0d99dca3d84a31dd1704a4e3fabed0bb684966c
shruti202X <shrutibansal3.22@gmail.com>
2024-10-15 13:34:16 +0530

    Implemented Garbage Collector

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from segment import Segmentation
 from datasets.dataset import Dataset
 from argparse import ArgumentParser
 import torch
+import gc
 
 parser = ArgumentParser()
 parser.add_argument("--bs", type=bool, default=False)
@@ -23,7 +24,13 @@ if __name__ == '__main__':
     print(device)
 
     seg = Segmentation(args.process, args.bs, args.epochs, tuple(args.resolution), args.activation, args.loss_type, args.threshold, args.conv_type)
+
+    torch.cuda.empty_cache()
+    gc.collect()
+
     ds = Dataset(args.dataset)
+
+    print("No. IoU mIoU")
 
     total_iou = 0
     total_samples = 0
@@ -34,9 +41,14 @@ if __name__ == '__main__':
                 total_iou += iou
                 total_samples += 1
                 print(f"{total_samples} {iou:.2f} {(total_iou/total_samples):.2f}")
-
             except Exception as e:
                 print(e)
                 continue
+            torch.cuda.empty_cache()
+            gc.collect()
+        torch.cuda.empty_cache()
+        gc.collect()
     
     print(f'Final mIoU: {(total_iou / total_samples):.4f}')
+    torch.cuda.empty_cache()
+    gc.collect()
