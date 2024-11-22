b8c61eb6dbe3c39a503cd0548c341b2bce907fc2
shruti202X <shrutibansal3.22@gmail.com>
2024-11-22 12:55:23 +0530

    Used imageio library to display the tif file.

--- debug.py
+++ debug.py
@@ -1,6 +1,7 @@
 import os
 import torch
 from PIL import Image
+import imageio.v2 as imageio
 os.environ['TORCH'] = torch.__version__
 
 # print(torch.__version__) # 2.4.1+cu118
@@ -19,4 +20,14 @@ masks = masks[1:-1]
 
 # print(len(masks), len(images)) # 610 610
 
-Image.open(masks[5])
+#Image.open(masks[5])
+
+file_path = images[22]
+img_array = imageio.imread(file_path)
+# print(img_array.shape) # (288, 384, 3)
+
+# Convert the NumPy array to a PIL Image
+pil_image = Image.fromarray(img_array)
+
+# Display the image or perform further processing
+pil_image.show()
